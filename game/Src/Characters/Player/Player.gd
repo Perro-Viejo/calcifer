6601b662995e6d7e3ef66c9bff17d3f95a848a9e
Carenalga <mapedorr@gmail.com>
2020-04-18 07:01:00 -0500

    Added temporary tilemap.
    The viewport follows the player character.

--- game/Src/Characters/Player/Player.gd
+++ game/Src/Characters/Player/Player.gd
@@ -1,9 +1,14 @@
 extends KinematicBody2D
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Variables ░░░░
-var speed = 2 * 60
+export(float) var speed = 2
+
+onready var _calc_speed: float = speed * 60
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Funciones ░░░░
+func _ready() -> void:
+	pass
+
 func _process(delta):
 	var dir = Vector2.ZERO
 	dir.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	dir.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
-	translate(dir * speed * delta)
+	translate(dir * _calc_speed * delta)
