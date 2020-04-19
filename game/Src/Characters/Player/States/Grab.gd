f0ee68bc3de8e5470303b32ec8b851aef44785e5
Carenalga <mapedorr@gmail.com>
2020-04-18 22:32:09 -0500

    Pickables can hide one Pickable

--- game/Src/Characters/Player/States/Grab.gd
+++ game/Src/Characters/Player/States/Grab.gd
@@ -9,5 +9,6 @@ func enter(msg: Dictionary = {}) -> void:
 	_owner.can_grab.z_index = _owner.z_index + 1
 	_owner.grabbing = true
 	(_owner.can_grab as Pickable).being_grabbed = true
+	print(_owner.can_grab.name)
 
 	_owner.play_animation(_owner.STATES.GRAB)
--- game/Src/Pickables/Pickable.gd
+++ game/Src/Pickables/Pickable.gd
@@ -1,14 +1,31 @@
+tool
 class_name Pickable
 extends Area2D
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Variables ░░░░
 export(bool) var is_good = true
 export(int) var carbs = 2
+export(Texture) var img setget set_sprite_texture
+export(String) var on_free = ''
 
 var being_grabbed: bool = false setget set_being_grabbed
+
+var _hides: Pickable
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Funciones ░░░░
 func _ready() -> void:
 	connect('area_entered', self, '_check_collision', [ true ])
 	connect('area_exited', self, '_check_collision')
+
+	for child in get_children():
+		if child.is_in_group('Pickable'):
+			_hides = child as Pickable
+
+			_hides.hide() # ja!
+			_hides.toggle_collision(false)
+
+
+func set_sprite_texture(tex: Texture) -> void:
+	img = tex
+	$Sprite.texture = img
 
 
 func set_being_grabbed(new_val: bool) -> void:
@@ -19,11 +36,34 @@ func set_being_grabbed(new_val: bool) -> void:
 	# del objeto y se lo comerá
 	monitorable = !new_val
 
+	# Sacar el objeto ocultiño
+	if _hides:
+		var dup: Pickable = _hides.duplicate() as Pickable
+		dup.global_position = global_position
+		dup.visible = true
+		dup.toggle_collision()
+		if dup.on_free != '':
+			Event.emit_signal('character_spoke', 'Pickable', tr(dup.on_free))
+
+		get_parent().call_deferred('add_child', dup)
+
+		_hides.queue_free()
+
+		_hides = null
+
+
+func toggle_collision(enable: bool = true) -> void:
+	$CollisionShape2D.disabled = !enable
+
 
 func _check_collision(area: Area2D, grab: bool = false) -> void:
 	if area.name != 'Player': return
 
+	var player: Player = area as Player
+
+	if player.grabbing: return
+
 	if grab:
-		(area as Player).can_grab = self
+		player.can_grab = self
 	else:
-		(area as Player).can_grab = null
+		player.can_grab = null
